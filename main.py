47412d6ec0d5807e6547f485ceeaaa5e89757cd5
Bia Anton <biancabub@yahoo.com>
2021-10-13 13:55:02 +0300

    Laborator 3

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from typing import List
 def menu():
         print('1.Citire date')
         print('2.Determinare cea mai lunga subsecventa cu proprietatea ca toate numerele sunt palindroame')
-        print('3.Determinarea cea mai lunga subsecventa cu proprietatea ca toate numerele se pot scrie ca x**k, k citit, x intreg pozitiv')
-        print('4.Iesire din program')
+        print('3.Determinare cea mai lunga subsecventa cu proprietatea ca toate numerele se pot scrie ca x**k, k citit, x intreg pozitiv')
+        print('4.Determinare cea mai lunga subsecventa cu proprietatea ca toate numerele sunt in progresie aritmetica')
+        print('5.Iesire din program')
 
 
 def citire_lista() -> List[int]:
@@ -105,6 +106,41 @@ def test_get_longest_powers_of_k():
         assert get_longest_powers_of_k([1,8,27,64,2,27],3)==[8,27,64]
 
 
+def get_progresie_aritmetica(lst: List[int])->List[int]:
+        '''
+        Verifica daca elementele din lista sunt in progresie aritmetica
+        :param lst: lista din care verificam numerele
+        :return: valoarea 1 daca elementele din lista sunt in progresie aritmetica, respectiv 0 in caz contrar
+        '''
+        n=len(lst)
+        for i in range (1,n-1):
+                if lst[i]!= (lst[i-1]+lst[i+1])/2: return 0
+        return 1
+
+
+def get_longest_arithmetic_progression(lst: List[int]) -> List[int]:
+        '''
+        Determina cea mai lunga subsecventa cu proprietatea ca toate numerele sunt in progresie aritmetica
+        :param lst: lista in care cautam subsecventa
+        :return: returnam subsecventa dorita
+        '''
+        n = len(lst)
+        lista=[]
+        result = []
+        for left in range(0,n-2):
+                for right in range(left+3, n+1):
+                        if get_progresie_aritmetica(lst[left:right])== 1:
+                                lista.append(lst[left:right])
+        for num in lista:
+                if(len(num)>len(result)):
+                        result= num
+        return result
+
+def test_get_longest_arithmetic_progression():
+        assert get_longest_arithmetic_progression([1,2,3,5,9,10,11,12,13])==[9,10,11,12,13]
+        assert get_longest_arithmetic_progression([3,2,5,6,7,8,10,11,12])==[5,6,7,8]
+        assert get_longest_arithmetic_progression([3,1,2,1,5,7,8,9])==[7,8,9]
+
 def main():
         lst = []
         while True:
@@ -117,7 +153,9 @@ def main():
                 elif opt == '3':
                         putere=int(input('Dati valoarea lui k: '))
                         print(f'Cea mai lunga subsecventa de numere care se pot scrie sub forma x la k este: {get_longest_powers_of_k(lst,putere)}.')
-                elif opt == '4':
+                elif opt== '4':
+                        print(f'Cea mai lunga subsecventa cu proprietatea ca toate numerele sunt in progresie aritmetica este: {get_longest_arithmetic_progression(lst)}.')
+                elif opt == '5':
                         break
                 else: print('Optiune invalida.')
 
@@ -126,5 +164,6 @@ if __name__ == '__main__':
         main()
         test_get_longest_all_palindromes()
         test_get_longest_powers_of_k()
+        test_get_longest_arithmetic_progression()
 
 
